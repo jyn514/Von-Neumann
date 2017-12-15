1e1f90ea24a7f5352bbaad19ff752ff6cbae866d
Nathan Faucett <nathanfaucett@gmail.com>
2017-12-15 12:19:27 -0600

    fix test

--- src/executable_memory.rs
+++ src/executable_memory.rs
@@ -152,12 +152,15 @@ mod test {
 
         memory[0] = 0xb8;
         memory[1] = 0xff;
-        memory[2] = 0xc3;
+        memory[2] = 0xff;
+        memory[3] = 0xff;
+        memory[4] = 0xff;
+        memory[5] = 0xc3;
 
-        let f: fn() -> u8 = unsafe {
-            mem::transmute((&memory[0..3]).as_ptr())
+        let f: fn() -> u32 = unsafe {
+            mem::transmute((&memory[0..6]).as_ptr())
         };
 
-        assert_eq!(f(), 255);
+        assert_eq!(f(), 4294967295);
     }
 }
