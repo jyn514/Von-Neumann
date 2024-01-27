b6cbe046b4e97bfbabfeafefbfdb6350a46d923b
jyn <github@jyn.dev>
2024-01-27 15:01:33 -0500

    remove duplicate test

--- src/bad_vec.rs
+++ src/bad_vec.rs
@@ -110,22 +110,6 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_call_function() {
-        let mut memory = ExecutableMemory::new(1);
-
-        memory[0] = 0xb8;
-        memory[1] = 0xff;
-        memory[2] = 0xff;
-        memory[3] = 0xff;
-        memory[4] = 0xff;
-        memory[5] = 0xc3;
-
-        let f: fn() -> u32 = unsafe { core::mem::transmute(memory[0..6].as_ptr()) };
-
-        assert_eq!(f(), 4294967295);
-    }
-
-    #[test]
     #[should_panic = "don't try to allocate usize::MAX lol"]
     fn overflow() {
         ExecutableMemory::new(usize::MAX);
